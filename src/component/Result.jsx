ef7a14135cd43ad4e2a8476cf8882a30888a21f8
ankit.vishnoi <ankit.vishnoi@servicenow.com>
2020-06-20 20:10:57 +0530

    my website

---
+++ src/component/Result.jsx
@@ -0,0 +1,203 @@
+import React, { Component, Fragment } from "react";
+
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+const color =["blue", "green"];
+
+function colorSelection(index){
+    if(index>=0 && index<=5)
+        return "red";
+    var num = index - 6;
+    if((num/9)%2 == 0)
+        return  color[0];
+    else
+        return color[1];
+}
+
+export default class Result extends Component {
+  calculateResult = () => {
+    const {
+      amount,
+      days,
+      mateWater,
+      minLength,
+      maxLength,
+      minWidth,
+      maxWidth,
+      minHeight,
+      maxHeight,
+    } = this.props.rawData;
+
+    let constants = {
+        perDayWage: 190,
+        rate: 118.62,
+    };
+   
+    constants['mwQuantity'] = Math.round(amount/(days * constants.perDayWage * 50));
+
+    let renderBody = [];
+    renderBody.push(
+        <div className="row no-bottom-margin grey darken-4 white-text">
+            <div className="col border s1">No</div>
+            <div className="col border s2">Length</div>
+            <div className="col border s2">Width</div>
+            <div className="col border s2">Height</div>
+            <div className="col border s2">Result</div>
+            <div className="col border s3">Cumulative</div>
+        </div>
+    )
+
+    var mateVal = Math.round(constants.mwQuantity * constants.perDayWage * days);
+    // var mateVal = mate * constants.perDayWage * days;
+    console.log("mateVal", mateVal);
+
+    var waterVal = mateVal;
+    console.log("waterVal", waterVal);
+  
+    var excavation;
+    if(mateWater === "true"){
+        excavation = amount - 2 * mateVal;
+        console.log("excavation", excavation);
+    }else{
+        excavation = amount;
+        console.log("excavation", excavation);
+    }
+  
+    var quantity = parseFloat(excavation / constants.rate).toFixed(2);
+    console.log("quantity", quantity);
+  
+    let resultsTotal = 0;
+    let sum = 0, count = 0;
+    while (sum <= quantity) {
+      var length = randomNumber(minLength, maxLength).toFixed(1);
+      var width = randomNumber(minWidth, maxWidth).toFixed(1);
+      var height = randomNumber(minHeight, maxHeight).toFixed(2);
+      var result = parseFloat(length * width * height).toFixed(2);
+      result = parseFloat(result);
+      var oldSum = sum;
+      sum += result;
+      sum = parseFloat(parseFloat(sum).toFixed(2));
+     
+      
+      if (sum <= quantity) {
+         
+          renderBody.push(
+            <div className={`row no-bottom-margin ${colorSelection(count)}`}>
+                <div className="col border s1">1</div>
+                <div className="col border s2">{String(length)}</div>
+                <div className="col border s2">{String(width)}</div>
+                <div className="col border s2">{String(height)}</div>
+                <div className="col border s2">{String(result)}</div>
+                <div className="col border s3">{String(sum)}</div>
+            </div>
+            )
+        console.log(
+          "1 * " +
+            length +
+            " * " +
+            width +
+            " * " +
+            height +
+            " = " +
+            result +
+            "     " +
+            sum
+        );
+        resultsTotal += parseFloat(result);
+      } else {
+        var remainderVal = quantity - oldSum;
+        remainderVal = parseFloat(parseFloat(remainderVal).toFixed(2));
+        length = randomNumber(minLength, maxLength).toFixed(1);
+        height = randomNumber(minHeight, maxHeight).toFixed(2);
+  
+        width = remainderVal / (length * height);
+  
+        result = parseFloat(length * width * height);
+  
+        console.log("remainderVal", remainderVal);
+        renderBody.push(
+            <div className={`row no-bottom-margin ${colorSelection(count)}`}>
+                <div className="col border s1">1</div>
+                <div className="col border s2">{String(length)}</div>
+                <div className="col border s2">{String(width.toFixed(2))}</div>
+                <div className="col border s2">{String(height)}</div>
+                <div className="col border s2">{String(result.toFixed(2))}</div>
+                <div className="col border s3">{String(quantity)}</div>
+            </div>
+            )
+        console.log(
+          "1 * " +
+            length +
+            " * " +
+            width.toFixed(2) +
+            " * " +
+            height +
+            " = " +
+            result.toFixed(2) +
+            "     " +
+            quantity
+        );
+        resultsTotal += parseFloat(result);
+      }
+      count += 1;
+    }
+
+    renderBody.push(
+        <div className="row no-bottom-margin">
+            <div className="col border s7">Total Quantity</div>
+            <div className="col border s2">{String(resultsTotal.toFixed(2))}</div>
+            <div className="col border s3">{String(resultsTotal.toFixed(2))}</div>
+        </div>
+    )
+    renderBody.push(
+        <div className="row no-bottom-margin">
+            <div className="col border s9">@rate</div>
+            <div className="col border s3">{constants.rate}</div>
+        </div>
+    )
+    renderBody.push(
+        <div className="row no-bottom-margin">
+            <div className="col border s9">Excavation Amount</div>
+            <div className="col border s3">{excavation}</div>
+        </div>
+    )
+    mateWater=== "true" && renderBody.push(
+        <div className="row no-bottom-margin">
+            <div className="col border s9">Mate Amount</div>
+            <div className="col border s3">{mateVal}</div>
+        </div>
+    )
+    mateWater=== "true" && renderBody.push(
+        <div className="row no-bottom-margin">
+            <div className="col border s9">Water Amount</div>
+            <div className="col border s3">{mateVal}</div>
+        </div>
+    )
+    
+    renderBody.push(
+        <Fragment>
+        <div className="row no-bottom-margin"></div>
+        <div className="row no-bottom-margin">
+            <div className="col border s9">Total Iterations</div>
+            <div className="col border s3">{count}</div>
+        </div>
+        </Fragment>
+    )
+   
+    return (
+        <div>
+            {renderBody}
+        </div>
+    )
+  };
+
+  render() {
+    return (
+        <div>
+            {this.calculateResult()}
+        </div>
+    );
+  }
+}
