2e6eede407b1a35fd4bd6f5741a67eb6cccce70b
akkibot <akhilesh.singh.skps@gmail.com>
2021-04-07 21:10:03 +0530

    Updated Website

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c4433ad12edf9d5a262b9ae46265465.js"
+  "/precache-manifest.1c63e675cda424500a0178b87e617e44.js"
 );
 
 self.addEventListener('message', (event) => {
